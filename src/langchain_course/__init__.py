4a33750f075a34990a86184d70fcca3f826df913
Haneef <haneef@Haneefs-MacBook-Pro.local>
2026-09-10 10:09:44 +0800

    Implement structured output to pydantic class

--- src/langchain_course/__init__.py
+++ src/langchain_course/__init__.py
@@ -1,4 +1,6 @@
-from asyncio import tools
+from typing import List
+
+from pydantic import BaseModel, Field
 
 from dotenv import load_dotenv
 
@@ -6,11 +8,21 @@ load_dotenv()
 
 # need to give tools and llm to create_agent
 from langchain.agents import create_agent
-from langchain.tools import tool
 from langchain_core.messages import HumanMessage
-from langchain_ollama import ChatOllama
-
+# from langchain_ollama import ChatOllama
+from langchain_groq import ChatGroq
 from langchain_tavily import TavilySearch
+
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+
+    url:str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for agent response  with answer and sources"""
+
+    answer:str = Field(description="Agent's answer to the query")
+    sources:List[Source] = Field(default_factory=list, description="List of sources to the answer")
 
 # Custom Tool for searching over the internet using Tavily API
 # from tavily import TavilyClient
@@ -35,13 +47,12 @@ from langchain_tavily import TavilySearch
 def main() -> None:
     print("Hello from langchain-course!")
 
-    llm = ChatOllama(model="qwen2.5",temperature=1)
-
+    llm = ChatGroq(model="qwen/qwen3.8-27b",temperature=0.3)
     tools = [TavilySearch()]
 
-    agent = create_agent(model=llm, tools=tools)
+    agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
-    result = agent.invoke({ "messages": HumanMessage(content="What is the weather in Seksen 6, Kota Damansara, Malaysia?")})
+    result = agent.invoke({ "messages": HumanMessage(content="What is the weather in Kota Damansara, Malaysia?")}) 
     print(result)
 
 if __name__ == "__main__":
